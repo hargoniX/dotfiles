4e51189ad303a3c87c438ab5a7a91a197183d4d3
Henrik Böving <hargonix@gmail.com>
2021-02-01 16:35:49 +0100

    a bit of config, especially new snippets + org mode stuff

--- dotfiles/init.el
+++ dotfiles/init.el
@@ -5,7 +5,7 @@
 (toggle-scroll-bar -1) ;; Or this
 (setq inhibit-startup-screen t) ;; Leave me alone with your tutorials
 (setq tramp-default-method "ssh") ;; speed up tramp mode
-(set-face-attribute 'default nil :height 130) ;; Set font size
+(set-face-attribute 'default nil :height 125) ;; Set font size
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -43,12 +43,16 @@
 (eval-when-compile
   (require 'use-package))
 
-(setq use-package-always-ensure t) ; always download packages
+(use-package quelpa
+  :ensure t)
+(use-package quelpa-use-package
+  :ensure t)
 
 ;; General things
 
 ;; Themes and icons
 (use-package doom-themes
+  :ensure t
   :init
   (load-theme 'doom-gruvbox t))
 
@@ -56,38 +60,47 @@
 
 ;; auto indent change like vim sleuth
 (use-package dtrt-indent
-  :init
-  (add-hook 'prog-mode-hook 'dtrt-indent-mode))
+  :ensure t
+  :hook
+  (prog-mode . dtrt-indent-mode)
+  (markdown-mode . dtrt-indent-mode))
 
 ;; 80 charcater limit line in prog mode
 (use-package fill-column-indicator
+  :ensure t
   :diminish fci-mode
   :config
   (setq fci-rule-width 1)
   (setq fci-rule-color "red")
-  :init
-  (add-hook 'prog-mode-hook 'fci-mode))
+  :hook
+  (prog-mode . fci-mode)
+  (markdown-mode . fci-mode))
 
 (use-package smartparens ;; auto parens
+  :ensure t
   :diminish smartparens-mode
   :config
   (require 'smartparens-config)
   (setq sp-highlight-pair-overlay nil) ;; to hide this fucking highlighting
-  :init
-  (add-hook 'prog-mode-hook 'smartparens-mode))
+  :hook
+  (prog-mode . smartparens-mode)
+  (markdown-mode . smartparens-mode))
 
 (use-package general
+  :ensure t
   :init
   ;; Space as leader key
   (general-create-definer vim-leader-def :prefix "SPC"))
 
 (use-package which-key
+  :ensure t
   :init (which-key-mode)
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 1))
 
 (use-package evil
+  :ensure t
   :init
   (setq evil-toggle-key "C-~") ;; so C-z works for background
   (setq evil-want-C-d-scroll t)
@@ -97,6 +110,7 @@
   (evil-mode))
 
 (use-package ivy
+  :ensure t
   :diminish
   :bind (("C-s" . swiper) ; TODO: possibly map this to / at some point?
          :map ivy-minibuffer-map
@@ -115,10 +129,12 @@
   (ivy-mode 1))
 
 (use-package doom-modeline
+  :ensure t
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
 
 (use-package counsel
+  :ensure t
   :bind (("M-x" . counsel-M-x)
          ("C-x b" . counsel-ibuffer)
          ("C-x C-f" . counsel-find-file)
@@ -128,6 +144,7 @@
 ;; Org mode stuff
 
 (use-package org
+  :ensure t
   ;; M-LEFT M-RIGHT deindent indent node
   ;; M-UP M-DOWN move node up down
   ;; Tab fold/unfold
@@ -143,11 +160,14 @@
     "oci" 'org-clock-in
     "oco" 'org-clock-out
     "oy"  'org-cycle) ;; Org-cYcle
+  :config
+  (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.5))
   :init
   (setq org-todo-keywords '((sequence "TODO" "PROGRESS" "FEEDBACK" "|" "DONE" "DELEGATED")))
   (setq org-log-done 'time))
 
 (use-package org-bullets
+  :ensure t
   :after org
   :hook (org-mode . org-bullets-mode)
   :custom
@@ -158,6 +178,7 @@
 ;; Development stuff
 
 (use-package magit
+  :ensure t
   :general
   (vim-leader-def 'normal 'global
     "gb" 'magit-blame ;; git blame
@@ -167,17 +188,21 @@
     "ga" 'magit-branch)) ;; git ast (as b is taken)
 
 (use-package evil-magit
+  :ensure t
   :after magit)
 
 (use-package lsp-mode
+  :ensure t
   :commands (lsp lsp-deferred)
   :init
   (setq lsp-keymap-prefix "C-l")
+  (setq gc-cons-threshold 100000000) ;; 100 mb
+  (setq read-process-output-max (* 1024 1024)) ;; 1mb
   :config
   (lsp-enable-which-key-integration t)
   (setq lsp-rust-server 'rust-analyzer)
-  (setq lsp-log-io t)
   (setq lsp-auto-guess-root t)
+  (setq lsp-idle-delay 1.)
   :hook
   (rust-mode . lsp)
   (java-mode . lsp)
@@ -186,12 +211,12 @@
     "gd" 'lsp-find-definition))
 
 (use-package company
+  :ensure t
   :hook
   (lsp-mode . company-mode)
   (prog-mode . company-mode)
   (LaTeX-mode . company-mode)
-  :init
-  (setq gc-cons-threshold 1600000)
+  (org-mode . company-mode)
   :custom
   (company-minimum-prefix-length 2)
   (company-idle-delay 0.4)
@@ -202,9 +227,11 @@
 	      ))
 
 (use-package company-box
+  :ensure t
   :hook (company-mode . company-box-mode))
 
-(use-package projectile)
+(use-package projectile
+  :ensure t)
 
 (defun company-mode/backend-with-yas (backend)
   (if (and (listp backend) (member 'company-yasnippet backend))
@@ -216,15 +243,18 @@
   (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends)))
 
 (use-package yasnippet
+  :ensure t
   :init
   :hook
   (company-mode . yas-minor-mode)
   (company-mode . company-mode/add-yasnippet))
 
-(use-package yasnippet-snippets)
+(use-package yasnippet-snippets
+  :quelpa ((yasnippet-snippets :fetcher github :repo "hargoniX/yasnippet-snippets") :upgrade t))
 
 ;; rust
 (use-package rust-mode
+  :ensure t
   :hook
   (rust-mode . prettify-symbols-mode)
   (rust-mode . (lambda ()
@@ -236,6 +266,7 @@
 
 ;; LaTeX
 (use-package auctex
+  :ensure t
   :defer t
   :init
   (setq TeX-auto-save t)
@@ -244,6 +275,7 @@
 
 ;; Java
 (use-package lsp-java
+  :ensure t
   :config
   (setq lsp-java-format-on-type-enabled nil))
 
@@ -252,3 +284,15 @@
 		 (push '("!=" . ?≠) prettify-symbols-alist)
 		 (push '("<=" . ?≤) prettify-symbols-alist)
                  (push '(">=" . ?≥) prettify-symbols-alist)))
+
+;; Haskell
+(use-package haskell-mode
+  :ensure t
+  :hook
+  (haskell-mode . interactive-haskell-mode))
+
+(use-package lsp-haskell
+  :ensure t
+  :hook
+  (haskell-mode . lsp)
+  (haskell-literate-mode . lsp))
