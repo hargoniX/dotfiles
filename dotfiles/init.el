afbce232d4c8cfcc5a661103bbeb3fb60e6a805b
Henrik Böving <hargonix@gmail.com>
2022-01-24 01:48:00 +0100

    fix time and lean4 git remote

--- dotfiles/init.el
+++ dotfiles/init.el
@@ -494,8 +494,7 @@
 
 
 (use-package lean4-mode
-  :straight (lean4-mode :type git :host github :repo "leanprover/lean4"
-             :files ("lean4-mode/lean4*.el"))
+  :straight (lean4-mode :type git :host github :repo "leanprover/lean4-mode")
   ;; to defer loading the package until required
   :commands (lean4-mode))
 
