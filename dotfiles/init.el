68035fcb958140f8f8ecc5709434df73264e17d1
Henrik Böving <hargonix@gmail.com>
2022-05-09 14:34:53 +0200

    pimp flycheck

--- dotfiles/init.el
+++ dotfiles/init.el
@@ -403,26 +403,14 @@
 
 (use-package flycheck
   :straight t
-  :after lsp)
+  )
 
-(use-package quick-peek
+(use-package flycheck-posframe
   :straight t
-  :after lsp)
-
-(use-package flycheck-inline
-  :straight t
-  :after quick-peek
   :hook
-  (flycheck-mode . flycheck-inline-mode)
+  (flycheck-mode . flycheck-posframe-mode)
   :config
-  (setq flycheck-inline-display-function
-      (lambda (msg pos err)
-        (let* ((ov (quick-peek-overlay-ensure-at pos))
-               (contents (quick-peek-overlay-contents ov)))
-          (setf (quick-peek-overlay-contents ov)
-                (concat contents (when contents "\n") msg))
-          (quick-peek-update ov)))
-      flycheck-inline-clear-function #'quick-peek-hide))
+  (flycheck-posframe-configure-pretty-defaults))
 
 (use-package company
   :straight t
