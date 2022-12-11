8167a0b2e59c2d3b8fe02d35f2c93da410c1d3fa
Henrik Böving <hargonix@gmail.com>
2022-12-11 17:31:07 +0100

    feat: support abbreviations.json

--- dotfiles/init.el
+++ dotfiles/init.el
@@ -754,7 +754,11 @@ Or interactively enable it in a buffer."
   (haskell-literate-mode . lsp))
 
 (use-package lean4-mode
-  :straight (lean4-mode :type git :host github :repo "leanprover/lean4-mode")
+  :straight (lean4-mode
+	     :type git
+	     :host github
+	     :repo "leanprover/lean4-mode"
+	     :files ("*.el" "data"))
   ;; to defer loading the package until required
   :commands (lean4-mode))
 
