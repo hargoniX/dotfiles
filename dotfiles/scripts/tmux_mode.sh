319a114c100dfb5cc239edb54212ff1e79a1ff72
Henrik Böving <hargonix@gmail.com>
2023-09-29 09:34:59 +0200

    chore: delete unused scripts

--- dotfiles/scripts/tmux_mode.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ $( xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) -notype _NET_WM_NAME | cut -d " "  -f 3) = '"tmux"' ]; then
-        i3-msg "mode tmux"
-else
-        i3-msg "mode default"
-fi
