efd9b98e19820bb639e2012b332e2342c28d9c95
Henrik Böving <hargonix@gmail.com>
2023-10-02 01:16:49 +0200

    feat: nvim snippets and indent

--- dotfiles/nvim/lua/hboeving/init.lua
+++ dotfiles/nvim/lua/hboeving/init.lua
@@ -24,8 +24,12 @@ require("lazy").setup({
     end,
   },
   {
-    "tpope/vim-sleuth",
-    event = "InsertEnter",
+    "nmac427/guess-indent.nvim",
+    lazy = false,
+    priority = 999,
+    config = function()
+      require("guess-indent").setup {}
+    end,
   },
   {
     "linrongbin16/lsp-progress.nvim",
@@ -78,14 +82,16 @@ require("lazy").setup({
     "L3MON4D3/LuaSnip",
     -- follow latest release.
     version = "2.*",
-    lazy = true,
+    event = "InsertEnter",
+    config = function()
+      require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "~/.config/nvim/lua/hboeving/snippets/" } })
+    end
   },
   {
     "Julian/lean.nvim",
     ft = "lean",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      -- you also will likely want nvim-cmp or some completion engine
     },
 
     -- see details below for full configuration options
@@ -177,9 +183,17 @@ require("lazy").setup({
       "petertriho/cmp-git",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-nvim-lsp",
+      "saadparwaiz1/cmp_luasnip"
     },
     config = function()
       local cmp = require"cmp"
+      local luasnip = require("luasnip")
+
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
 
       cmp.setup {
         snippet = {
@@ -192,12 +206,35 @@ require("lazy").setup({
           { name = "nvim_lsp" },
           { name = "luasnip" },
           { name = "treesitter" },
+          { name = "luasnip" },
           { name = "async_path" },
         }, {
           { name = "buffer" }
         }),
 
         mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+            -- they way you will only jump inside the snippet region
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
